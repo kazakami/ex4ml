76fac2943f6ffcfe649775b7754cf5c1497971f6
kazakami <kazakami@kmc.gr.jp>
2015-01-09 18:00:29 +0900

    型推論のための単一化を行う関数を書いた。

--- sample/typing.ml
+++ sample/typing.ml
@@ -19,7 +19,32 @@ let rec subst_type l = function
 	else subst_type xs t)
   | TyFun (t1, t2) -> TyFun ((subst_type l t1), (subst_type l t2))
   | x -> x
-      
+
+let eqs_of_subst s =
+  List.map (fun (tv, t) -> (TyVar tv, t)) s
+
+let rec subst_eqs s eqs =
+  let rec subst (a, t) =
+    function
+      [] -> []
+    | (TyVar t1, TyVar t2) :: xs -> if t1 = a && t2 = a
+				    then (t, t) :: (subst (a, t) xs)
+				    else if t1 == a
+				    then (t, TyVar t2) :: (subst (a, t) xs)
+				    else if t2 == a
+				    then (TyVar t2, t) :: (subst (a, t) xs)
+				    else (TyVar t1, TyVar t2) :: (subst (a, t) xs)
+    | (TyVar t1, tt) :: xs -> if t1 = a
+			      then (t, tt) :: (subst (a, t) xs)
+			      else (TyVar t1, tt) :: (subst (a, t) xs)
+    | (tt, TyVar t2) :: xs -> if t2 = a
+			      then (tt, t) :: (subst (a, t) xs)
+			      else (tt, TyVar t2) :: (subst (a, t) xs)
+    | ttt :: xs -> ttt :: (subst (a, t) xs)
+  in match s with
+       [] -> []
+     | (a, t) :: xs -> subst_eqs xs (subst (a, t) eqs)
+	   
 let ty_prim op ty1 ty2 =
   match op with
     Plus | Minus | Mult | Div ->
@@ -63,3 +88,20 @@ let rec ty_exp tyenv = function
 let ty_decl tyenv = function
     Exp e -> [ty_exp tyenv e]
   | _ -> err ("Not Implemented in decl!")
+
+
+let rec unify =
+  function
+    [] -> []
+  | (TyInt, TyInt) :: xs -> xs
+  | (TyBool, TyBool) :: xs -> xs
+  | (TyVar a, t) :: xs -> if MySet.member a (freevar_ty t)
+			  then err ("err in unify")
+			  else (TyVar a, t) :: (unify (subst_eqs [(a, t)] xs))
+  | (t, TyVar a) :: xs -> unify ((TyVar a, t) :: xs)
+  | _ -> err "err in unify"
+
+
+
+
+	     
